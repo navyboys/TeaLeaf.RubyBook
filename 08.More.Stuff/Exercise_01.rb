77e527634f4be6423cc4f5696d9c20a8f43ae7ce
navy <navyboys@gmail.com>
2014-05-02 13:06:14 +0800

    complete - More Stuff!

---
+++ 08.More.Stuff/Exercise_01.rb
@@ -0,0 +1,20 @@
+words = ["laboratory", "experiment", "Pans Labyrinth", "elaborate", "polar bear"]
+
+words.each do |word|
+  puts word if word =~ /lab/
+end
+
+# def check_in(word)
+#   if /lab/ =~ word
+#     puts word
+#   else
+#     puts "No match"
+#   end
+# end
+
+
+# check_in("laboratory")
+# check_in("experiment")
+# check_in("Pans Labyrinth")
+# check_in("elaborate")
+# check_in("polar bear")
